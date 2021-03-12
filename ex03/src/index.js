78f1ef9e0df442ee6f1551cf9fe4449e2481e88f
mhamzic <mhamzic@gmail.com>
2021-03-12 13:22:38 +0100

    Corrections v2

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -2,7 +2,7 @@ function main() {
   // Only change code below this line
 
   var myString;
-  myString = "FirstLine\n\tSecondLine\nThirdLine";
+  myString = "FirstLine\n\t\\SecondLine\nThirdLine";
 
   // Only change code above this line
 
